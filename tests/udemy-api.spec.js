5c2c4312d927e44d3378cac238274b0a54e755f0
Lucas <admlucasvieira@gmail.com>
2023-07-04 18:32:41 -0300

    test exercise adds

--- tests/udemy-api.spec.js
+++ tests/udemy-api.spec.js
@@ -4,9 +4,19 @@ const { test, expect } = require('@playwright/test');
 test.describe.parallel("API Testing", () => {
   const baseUrl = "https://reqres.in/api"
   
-  test("Simple API Test - Assert Response Status", async ({ request }) => {
+  test.only("Simple API Test - Assert Response Status", async ({ request }) => {
 
     const response = await request.get(`${baseUrl}/users?page=2`);
+    expect (response.status()).toBe(200);
+
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+
+  });
+
+  test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
+
+    const response = await request.get(`${baseUrl}/users?page=invalid endpont`);
     expect (response.status()).toBe(200);
 
   });
