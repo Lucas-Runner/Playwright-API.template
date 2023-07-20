7ad00ac0c726b9c557555bee7b2015a4a533495a
Lucas <admlucasvieira@gmail.com>
2023-07-19 23:29:31 -0300

    Post test added

--- tests/udemy-api.spec.js
+++ tests/udemy-api.spec.js
@@ -1,36 +1,48 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 test.describe.parallel("API Testing", () => {
-  const baseUrl = "https://reqres.in/api"
-  
-  test("Simple API Test - Assert Response Status", async ({ request }) => {
+    const baseUrl = "https://reqres.in/api"
 
-    const response = await request.get(`${baseUrl}/users?page=2`);
-    expect (response.status()).toBe(200);
+    test("Simple API Test - Assert Response Status", async ({request}) => {
 
-    const responseBody = JSON.parse(await response.text());
-    console.log(responseBody);
+        const response = await request.get(`${baseUrl}/users?page=2`);
+        expect(response.status()).toBe(200);
 
-  });
+        const responseBody = JSON.parse(await response.text());
+        console.log(responseBody);
 
-  test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
+    });
 
-    const response = await request.get(`${baseUrl}/users?page=invalid endpont`);
-    expect (response.status()).toBe(200);
+    test("Simple API Test - Assert Invalid Endpoint", async ({request}) => {
 
-  });
+        const response = await request.get(`${baseUrl}/users?page=invalid endpont`);
+        expect(response.status()).toBe(200);
 
-  test.only("GET request - Get User Detail", async ({ request }) => {
+    });
 
-    const response = await request.get(`${baseUrl}/users/1`);
-    const responseBody = JSON.parse(await response.text());
-    expect(response.status()).toBe(200);
-    expect(responseBody.data.id).toBe(1);
-    expect(responseBody.data.first_name).toBe("George");
-    expect(responseBody.data.last_name).toBe("Bluth");
-    console.log(responseBody);
+    test("GET request - Get User Detail", async ({request}) => {
 
-  });
-  
+        const response = await request.get(`${baseUrl}/users/1`);
+        const responseBody = JSON.parse(await response.text());
+        expect(response.status()).toBe(200);
+        expect(responseBody.data.id).toBe(1);
+        expect(responseBody.data.first_name).toBe("George");
+        expect(responseBody.data.last_name).toBe("Bluth");
+        console.log(responseBody);
+
+    });
+
+    test("POST request - Create New User", async ({request}) => {
+
+      const response = await request.post(`${baseUrl}/users`, {
+        data:{
+          id: 1000,
+        }
+      });
+
+      
+
+    });
+
 });
